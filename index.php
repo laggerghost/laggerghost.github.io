103b98f9c1a2de06980b6f4ac930514fe8e78db6
laggerghost <42460795+laggerghost@users.noreply.github.com>
2019-05-03 05:25:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<HTML>
+  <TITLE>L4663r666h05t</TITLE>
+    <style>
+		body{
+			background-image:url(http://pavbca.com/walldb/original/4/6/5/369807.jpg);
+			background-size:cover;
+			background-attachment: fixed;
+		}
+		p{
+			color:white;
+		}
+	</style>
