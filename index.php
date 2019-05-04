45e3b04c0c2d1c8b2d2762b76201a648aad3a7e7
laggerghost <42460795+laggerghost@users.noreply.github.com>
2019-05-04 20:04:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE HTML>
+<HTML LANG="ID">
+<HEAD>
+	<META CHARSET="UTF-8">
+	<TITLE>Malware - Home</TITLE>
+</HEAD>
+<STYLE>
+       body{
+	background-image:url(cover.jpg);
+	background-size:cover;
+	background-attachment: fixed;
+	}
+</STYLE>
+<BODY><PRE><FONT COLOR="SEASHELL">
+___  ___        _                              
+|  \/  |       | |                             
+| .  . |  __ _ | |__      __  __ _  _ __   ___ 
+| |\/| | / _` || |\ \ /\ / / / _` || '__| / _ \
+| |  | || (_| || | \ V  V / | (_| || |   |  __/
+\_|  |_/ \__,_||_|  \_/\_/   \__,_||_|    \___|
+                            De La Salle Manado
+
+               Coming Soon~</br>
+More Info:
+[-]<a href="https://m.me/laggerghozt">Developer</a>
+[-]<a href="mailto:wmlaurent@gmail.com">Mail Me</a>
